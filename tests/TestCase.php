87cc4cfe44e23eaf9c7fae6679569ae9cbdb08c1
martin <halaj@invelity.com>
2025-11-04 11:58:29 +0100

    Fix PHPUnit risky tests: restore error handlers in tearDown

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,13 +10,52 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    private $previousErrorHandler;
+
+    private $previousExceptionHandler;
+
     protected function setUp(): void
     {
+        $this->previousExceptionHandler = set_exception_handler(function ($e) {
+            throw $e;
+        });
+        if ($this->previousExceptionHandler !== null) {
+            set_exception_handler($this->previousExceptionHandler);
+        } else {
+            restore_exception_handler();
+        }
+
+        $this->previousErrorHandler = set_error_handler(function ($errno, $errstr, $errfile = null, $errline = null) {
+            return false;
+        });
+        if ($this->previousErrorHandler !== null) {
+            set_error_handler($this->previousErrorHandler);
+        } else {
+            restore_error_handler();
+        }
+
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
             fn (string $modelName) => 'Invelity\\WizardPackage\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
+    }
+
+    protected function tearDown(): void
+    {
+        if ($this->previousExceptionHandler !== null) {
+            set_exception_handler($this->previousExceptionHandler);
+        } else {
+            restore_exception_handler();
+        }
+
+        if ($this->previousErrorHandler !== null) {
+            set_error_handler($this->previousErrorHandler);
+        } else {
+            restore_error_handler();
+        }
+
+        parent::tearDown();
     }
 
     protected function getPackageProviders($app)
